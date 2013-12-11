de16c52bfeb454cfbbebe9a8e763891afa4ea04f
Jesicka Labud <jesicka@gmail.com>
2013-12-11 06:00:43 +0100

    Add statuses scaffold, add hover function using coffeescript, customize status layouts

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Commentclubbers::Application.routes.draw do
+  resources :statuses
+
   devise_for :users
   root "pages#home"
   get "about" => "pages#about" # creates about_path
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131205233447) do
+ActiveRecord::Schema.define(version: 20131211024927) do
+
+  create_table "statuses", force: true do |t|
+    t.string   "name"
+    t.text     "content"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "users", force: true do |t|
     t.string   "email",                  default: "", null: false
