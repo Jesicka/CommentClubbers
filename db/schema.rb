f11957cf7e6cf41cb4c31848f646218bff0634fb
Jesicka Labud <jesicka@gmail.com>
2013-12-13 18:49:35 +0100

    modify bootstrap and customization, header, add status page to navbar

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131211024927) do
+ActiveRecord::Schema.define(version: 20131213174212) do
 
   create_table "statuses", force: true do |t|
     t.string   "name"
@@ -33,6 +33,9 @@ ActiveRecord::Schema.define(version: 20131211024927) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "first_name"
+    t.string   "last_name"
+    t.string   "profile_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
