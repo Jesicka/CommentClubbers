7b7ae40661de4967aa26d0dc85f8590d61d57e2d
Jesicka Labud <jesicka@gmail.com>
2014-01-07 00:45:08 +0100

    Add profile to routes.rb, modify profiles controller to render current user profile, render 404 page if profile not found

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Commentclubbers::Application.routes.draw do
   root "pages#home"
   get "about" => "pages#about" # creates about_path
 
+  get '/:id', to: 'profiles#show'
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
