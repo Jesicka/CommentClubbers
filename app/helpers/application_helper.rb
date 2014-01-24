0e5d3a8ffd11f93fdcd6d465d1aa2bcc93ab51ba
Jesicka Labud <jesicka@gmail.com>
2014-01-24 04:04:17 +0100

    Add code to application helper file to make markdown work in statuses views

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,11 @@
 module ApplicationHelper
-	def markdown(text)
-		markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true)
+	def dat_markdown(text)
+	  options = {
+	    :autolink => true,
+	    :space_after_headers => true,
+	    :no_intra_emphasis => true
+	  }
+	  markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, options)
+	  markdown.render(text).html_safe
 	end
 end
--- app/helpers/statuses_helper.rb
+++ app/helpers/statuses_helper.rb
@@ -1,7 +1,2 @@
-module StatusesHelper
-
-		def markdown(text)
-		markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML, :autolink => true, :space_after_headers => true)
-	end
-	
+module StatusesHelper	
 end
